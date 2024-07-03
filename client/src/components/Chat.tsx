b59ca3024cae39a57dc6209a38498ee40d5cb3f2
Tomas Trujillo <tomastru114@gmail.com>
2024-07-03 16:58:21 +0200

    Feature: Upload images

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,18 +1,31 @@
-import { Button, Fade, IconButton, Input, Menu, MenuItem, Paper, TextField } from "@mui/material";
-import { useState } from "react";
+import { Button, Fade, IconButton, Input, Menu, MenuItem, Paper, styled, Modal, Box, CircularProgress } from "@mui/material";
+import { ChangeEvent, useState } from "react";
 import AddIcon from '@mui/icons-material/Add';
 import SendIcon from '@mui/icons-material/Send';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import axios from 'axios';
 import { Messages } from "../types/types";
 
-export function Chat() {
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+})
 
-    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
-    const open = Boolean(anchorEl)
+export function Chat() {
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
     const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorEl(event.currentTarget)
+        setAnchorEl(event.currentTarget);
     }
     const handleClose = () => {
-        setAnchorEl(null)
+        setAnchorEl(null);
     }
 
     const messages: Messages[] = [
@@ -22,27 +35,72 @@ export function Chat() {
         { message: "Holaaaaaaa", id: 2 }
     ]
 
+    const [selectedFile, setSelectedFile] = useState<File | null>(null)
+    const [openModal, setOpenModal] = useState(false)
+    const handleOpenModal = () => setOpenModal(true)
+    const handleCloseModal = () => setOpenModal(false)
+
+    const modalStyle = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+    }
+
+    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+
+        if (e.target.files) {
+            setSelectedFile(e.target.files[0])
+            handleOpenModal()
+            handleClose()
+        }
+    }
+
+    const handleUploadImage = async () => {
+        if (!selectedFile) return
+
+        const formData = new FormData()
+        formData.append('file', selectedFile)
+        formData.append('sender', "Tomas")
+        formData.append('recipient', "Maya")
+
+        try {
+            const response = await axios.post('/messages/image', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            })
+            console.log('Imagen subida con éxito:', response.data)
+            setSelectedFile(null)
+            handleCloseModal()
+        } catch (error) {
+            console.error('Error subiendo la imagen', error)
+        }
+    }
+
     return (
         <div className="flex flex-col w-full h-full justify-end items-end">
             <div className="w-full">
                 {messages.map((message, index) => (
                     <div key={index} className="w-full">
                         {message.id == 1
-                            ?
-                            (
+                            ? (
                                 <article className="flex w-auto justify-end">
                                     <Paper className="p-5 m-2"
-                                    sx={{
-                                        backgroundColor: "#2563eb",
-                                        color: "white"
-                                    }}
+                                        sx={{
+                                            backgroundColor: "#2563eb",
+                                            color: "white"
+                                        }}
                                     >
                                         <span>{message.message}</span>
                                     </Paper>
                                 </article>
-                            )
-                            :
-                            (
+                            ) : (
                                 <article className="flex w-auto justify-start">
                                     <Paper className="p-5 m-2">
                                         <span>{message.message}</span>
@@ -75,17 +133,88 @@ export function Chat() {
                         onClose={handleClose}
                         TransitionComponent={Fade}
                     >
-                        <MenuItem onClick={handleClose}>Profile</MenuItem>
-                        <MenuItem onClick={handleClose}>My account</MenuItem>
-                        <MenuItem onClick={handleClose}>Logout</MenuItem>
+                        <MenuItem>
+                            <Button
+                                component="label"
+                                role={undefined}
+                                variant="contained"
+                                tabIndex={-1}
+                                startIcon={<CloudUploadIcon />}
+                                sx={{
+                                    backgroundColor: "#2563eb",
+                                    '&:hover': {
+                                        backgroundColor: '#60a5fa',
+                                    },
+                                    color: "white"
+                                }}
+                            >
+                                Upload file
+                                <VisuallyHiddenInput type="file" onChange={handleFileChange} />
+                            </Button>
+                        </MenuItem>
                     </Menu>
                 </div>
                 <Input disableUnderline type="text" placeholder="type a message" className="w-full" />
-                <IconButton
-                >
+                <IconButton >
                     <SendIcon />
                 </IconButton>
-            </Paper>
-        </div>
-    )
+            </Paper >
+
+            <Modal
+                open={openModal}
+                onClose={handleCloseModal}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+                
+            >
+                <Box sx={modalStyle}>
+                    {selectedFile && (
+                        <div className="w-full">
+                            {URL.createObjectURL(selectedFile)
+                                ? (
+                                    <img
+                                        className="shadow-2xl"
+                                        src={URL.createObjectURL(selectedFile)}
+                                        alt="Selected Preview"
+                                        style={{ width: '100%', height: 'auto' }}
+                                    />
+                                )
+                                : (
+                                    <CircularProgress />
+                                )
+                            }
+                            <article className="w-full flex justify-around mt-5">
+                                <Button
+                                    component="label"
+                                    role={undefined}
+                                    variant="contained"
+                                    tabIndex={-1}
+                                    sx={{
+                                        backgroundColor: "#2563eb",
+                                        '&:hover': {
+                                            backgroundColor: '#60a5fa',
+                                        },
+                                        color: "white"
+                                    }}
+                                    onClick={handleUploadImage}
+                                >
+                                    Send
+                                </Button>
+                                <Button
+                                    component="label"
+                                    role={undefined}
+                                    variant="contained"
+                                    tabIndex={-1}
+                                    color={"error"}
+                                    onClick={handleCloseModal}
+                                >
+                                    Cancel
+                                </Button>
+                            </article>
+                        </div>
+                    )}
+                </Box>
+            </Modal>
+        </div >
+    );
 }
--- client/src/components/ChatsDrawer.tsx
+++ client/src/components/ChatsDrawer.tsx
@@ -10,11 +10,10 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import { useState } from 'react';
 import { Avatar } from '@mui/material';
 import DarkMode from './DarkModeSw';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 const drawerWidth = 240;
 
@@ -111,8 +110,7 @@ export default function ChatsDrawer() {
         </List>
         <Divider />
         <List className='h-full flex flex-col justify-end'>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
+            <ListItem disablePadding sx={{ display: 'block' }}>
               <ListItemButton
                 sx={{
                   minHeight: 48,
@@ -127,12 +125,11 @@ export default function ChatsDrawer() {
                     justifyContent: 'center',
                   }}
                 >
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  <SettingsIcon />
                 </ListItemIcon>
-                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+                <ListItemText primary={"Settings"} sx={{ opacity: open ? 1 : 0 }} />
               </ListItemButton>
             </ListItem>
-          ))}
         </List>
       </Drawer>
     </div>
